ba27e6e3f86fb76ce54c8169ece42f1495dfe356
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-09 18:38:31 +0700

    layout admin

---
+++ src/layouts/index.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <main className="bg-[#f7f7f7]  min-h-screen px-40 ">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
