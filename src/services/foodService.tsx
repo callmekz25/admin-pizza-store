b87e63e8f44d5ae58202b7aa7dc623edb3322aa5
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-09 18:39:59 +0700

    service to get all foods with supabase

---
+++ src/services/foodService.tsx
@@ -0,0 +1,15 @@
+import supabase from "../config/supabase.config";
+export const getFoods = async () => {
+  return await supabase.from("item").select(`
+    item_id,
+    item_name,
+    item_image,
+    description,
+    price,
+    category_id,
+    category:category_id (
+      category_name,
+      category_image
+    )
+  `);
+};
