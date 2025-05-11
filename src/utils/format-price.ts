9063697a1b6d66a1a61256231b71702576c59440
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-11 20:51:25 +0700

    utils to format price to vnd

---
+++ src/utils/format-price.ts
@@ -0,0 +1,8 @@
+const formatPrice = (price: number, type: boolean = true) => {
+  return type
+    ? price
+        .toLocaleString("vi-VN", { style: "currency", currency: "VND" })
+        .replace(/\./g, ",")
+    : price.toLocaleString("vi-VN", { style: "currency", currency: "VND" });
+};
+export default formatPrice;
