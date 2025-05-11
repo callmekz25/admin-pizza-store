5f732ab4e85153571f6e7681277ac5e4d223e8bb
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-11 20:51:16 +0700

    feat add routes for add product page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Layout from "../layouts";
-import Dashboard from "../pages/dashboard/Dashboard";
-import ProductsList from "../pages/product/ProductsList";
-import UserList from "../pages/user/UserList";
+import Dashboard from "../pages/dashboard/dashboard";
+import ProductsList from "../pages/product/products-list";
+import UserList from "../pages/user/users-list";
+import AddProduct from "../pages/product/add-product";
 const router = createBrowserRouter([
   {
     element: <Layout />,
@@ -31,6 +32,14 @@ const router = createBrowserRouter([
           </Suspense>
         ),
         path: "/users",
+      },
+      {
+        element: (
+          <Suspense fallback={<p>Loading...</p>}>
+            <AddProduct />
+          </Suspense>
+        ),
+        path: "/add-product",
       },
     ],
   },
