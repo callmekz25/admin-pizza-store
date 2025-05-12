81cad88ddb913d5cf21627a96ac9fcf2b2f2b061
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-12 20:46:19 +0700

    feat add hook get food by id & update food

--- src/hooks/food.ts
+++ src/hooks/food.ts
@@ -1,9 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-import { getFoods } from "../services/food-service";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getFoodById, getFoods, updateFood } from "../services/food-service";
 
 export const useGetFoods = () => {
   return useQuery({
     queryKey: ["foods"],
     queryFn: getFoods,
+  });
+};
+export const useGetFoodById = (id: string) => {
+  return useQuery({
+    queryKey: ["foods", id],
+    queryFn: () => getFoodById(id),
+    enabled: !!id,
+  });
+};
+export const useUpdateFood = () => {
+  return useMutation({
+    mutationFn: (data) => updateFood(data),
   });
 };
