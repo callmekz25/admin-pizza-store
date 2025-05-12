92a7139a4094b05c33e815e8bdfa2dfebd9a91f1
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-12 20:48:10 +0700

    feat add service to get data of food by id

--- src/services/food-service.ts
+++ src/services/food-service.ts
@@ -16,3 +16,11 @@ export const getFoods = async () => {
 export const upFood = async (food) => {
   return await supabase.from("item").insert([food]);
 };
+
+export const getFoodById = async (id: string) => {
+  return await supabase.from("item").select().eq("item_id", id).single();
+};
+export const updateFood = async (food) => {
+  const { item_id, ...data } = food;
+  return await supabase.from("item").update(data).eq("item_id", item_id);
+};
